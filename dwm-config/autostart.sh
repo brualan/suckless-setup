dc11770742c6c94485648c996c63ebf5fafabbab
Alexander Bruyako <aabruyako@gmail.com>
2020-04-01 23:54:22 +0300

    migrate to mawk, becouse it's binary smaller

--- dwm-config/autostart.sh
+++ dwm-config/autostart.sh
@@ -4,11 +4,11 @@ $HOME/utils/monitors/autochoose
 
 export _JAVA_AWT_WM_NONREPARENTING=1
 
-ID=`xinput list | awk '/Touchpad/ {print substr($6,4)}'`
+ID=`xinput list | mawk '/Touchpad/ {print substr($6,4)}'`
 
-TAPING_ID=`xinput list-props $ID | awk '/Tapping Enabled \(/ {print substr($4, 2, length($4)-3)}'`
-NATURAL_SCROLLING_ID=`xinput list-props $ID | awk '/Natural Scrolling Enabled \(/ {print substr($5, 2, length($5)-3)}'`
-MIDDLE_CLICK_ID=`xinput list-props $ID | awk '/Middle Emulation Enabled \(/ {print substr($5, 2, length($5)-3)}'`
+TAPING_ID=`xinput list-props $ID | mawk '/Tapping Enabled \(/ {print substr($4, 2, length($4)-3)}'`
+NATURAL_SCROLLING_ID=`xinput list-props $ID | mawk '/Natural Scrolling Enabled \(/ {print substr($5, 2, length($5)-3)}'`
+MIDDLE_CLICK_ID=`xinput list-props $ID | mawk '/Middle Emulation Enabled \(/ {print substr($5, 2, length($5)-3)}'`
 
 xinput set-prop $ID $TAPING_ID 1
 xinput set-prop $ID $NATURAL_SCROLLING_ID 1
@@ -19,7 +19,7 @@ OLD_STATUSBAR=""
 while sleep 1; do
 	DATETIME=`date +"%a %b %_d %H:%M"`
 	BATTERYSTATE=`for x in /sys/class/power_supply/BAT?/capacity; do cat $x; done`
-	VOLUME=`pactl list sinks | awk '/^\s*(Громкость|Volume)/ {print "Vol:" $5}'`
+	VOLUME=`pactl list sinks | mawk '/^\s*(Громкость|Volume)/ {print "Vol:" $5}'`
 	WIFI_NAME=`iwgetid -r`
 	FREE_MEM=`free | mawk '/^Mem/ { if ($7 < 700000) { print " !!! LOW MEMORY !!! ||" } else { print "" } }'`
 
