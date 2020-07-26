7fc4dc5b14b048a47c93e324994edd6ac95ed90b
Alexander Bruyako <aabruyako@gmail.com>
2020-07-26 12:47:11 +0300

    новые тэги для приложений

--- dwm-config/config.h
+++ dwm-config/config.h
@@ -29,7 +29,9 @@ static const Rule rules[] = {
 	/* class	   instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",          NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  	   NULL,       NULL,       1 << 2,       0,           -1 },
+	{ "Zoom",          NULL,       NULL,       1 << 6,       0,           -1 },
 	{ "Tor Browser",   NULL,       NULL,       1 << 7,       0,           -1 },
+	{ "Telegram",      NULL,       NULL,       1 << 8,       0,           -1 },
 };
 
 /* layout(s) */
